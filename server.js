55a5449328db4a22d1f2b3756ee2629078e2d80e
Dan Moneypenny <catullus1973@gmail.com>
2020-11-22 15:18:00 -0800

    Sweep away server files.

--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const app = require('./app/app');
-//const { PORT } = require('./app/config/config');
-const PORT = 8080;
-const server = http.createServer(app);
-
-server.listen(PORT, () => console.log(`*** Server is running on port ${PORT}... ***`));
