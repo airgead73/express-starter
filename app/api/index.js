95ed57a2eecb95f2b791a51d7df502fc565d1284
Dan Moneypenny <catullus1973@gmail.com>
2020-12-20 22:07:53 -0800

    Add template controller and routes.

--- app/api/index.js
+++ app/api/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const { usersRouter } = require('./routes/api.routes.users');
 const { projectsRouter } = require('./routes/api.routes.projects');
 const { itemsRouter } = require('./routes/api.routes.items');
+const { templatesRouter } = require('./routes/api.routes.templates');
 const apiRouter = Router();
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/projects', projectsRouter);
 apiRouter.use('/items', itemsRouter);
+apiRouter.use('/templates', templatesRouter);
 
 module.exports = {
   apiRouter,
