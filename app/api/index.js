2fa959dfa243e90dacaf63fec84b23f5f7227575
Dan Moneypenny <catullus1973@gmail.com>
2020-12-21 10:29:57 -0800

    Add resources controller, routes, and model.

--- app/api/index.js
+++ app/api/index.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 const { usersRouter } = require('./routes/api.routes.users');
 const { projectsRouter } = require('./routes/api.routes.projects');
 const { itemsRouter } = require('./routes/api.routes.items');
+const { resourcesRouter } = require('./routes/api.routes.resources');
 const { templatesRouter } = require('./routes/api.routes.templates');
 const apiRouter = Router();
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/projects', projectsRouter);
 apiRouter.use('/items', itemsRouter);
+apiRouter.use('/resources', resourcesRouter);
 apiRouter.use('/templates', templatesRouter);
 
 module.exports = {
