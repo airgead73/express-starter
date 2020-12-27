17ddf7fdbaf83810a35c8f5be8816838fa6b1700
Dan Moneypenny <catullus1973@gmail.com>
2020-12-26 23:29:12 -0800

    add delete client script

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -1,15 +1,18 @@
 import {login} from "./modules/login.js";
 import {signup} from "./modules/signup.js";
 import {projectAdd} from "./modules/project_add.js";
+import {contentDelete} from "./modules/delete.js"
 
 const d = document;
 const formLogin = d.getElementById('form_login');
 const formSignup = d.getElementById('signup');
 const formProjectAdd = d.getElementById('form_project_add');
+const btnsDelete = d.querySelectorAll('[data-delete]');
 
 if(formLogin) login(formLogin);
 if(formSignup) signup(formSignup);
 if(formProjectAdd) projectAdd(formProjectAdd);
+if(btnsDelete) contentDelete(btnsDelete);
 
 
 
